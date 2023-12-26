eeaa8716db65dc49413335005a4b613b11ec3d4f
madhav-singh-faujdar <madhavsinghf@gmail.com>
2023-12-26 17:35:57 +0530

    asd

--- src/Share.jsx
+++ src/Share.jsx
@@ -5,17 +5,35 @@ const ShareButton = () => {
     try {
       if (navigator.share) {
         await navigator.share({
-          title: 'Mongodb ',
-          text: 'Find out your best db',
-          url: 'https://www.mongodb.com/',
-          
+          title: 'Social viens ',
+          text: 'Find out your best influancer',
+          url: 'https://beta.socialveins.com/setting',
         });
       } else {
-        alert('Web Share API not supported in your browser');
+        // Fallback for browsers that do not support Web Share API
+        const shareableLink = 'https://beta.socialveins.com';
+
+        // You can customize the fallback method based on your requirements
+        // For example, opening a new window or copying to clipboard
+        const fallbackMethod = window.open ? openWindowFallback : copyToClipboardFallback;
+
+        fallbackMethod(shareableLink);
       }
     } catch (error) {
       console.error('Error sharing:', error.message);
     }
+  };
+
+  const openWindowFallback = (link) => {
+    // Open a new window or tab with the shareable link
+    window.open(link, '_blank');
+  };
+
+  const copyToClipboardFallback = (link) => {
+    // Fallback to copying the link to the clipboard
+    navigator.clipboard.writeText(link)
+      .then(() => alert('Link copied to clipboard'))
+      .catch((err) => console.error('Error copying to clipboard:', err));
   };
 
   return (
